077cecbb983108009b405ad74f34920ae7083abc
g4br-4d3v <gabrielalbuquerque-ads@hotmail.com>
2020-09-07 19:25:14 -0300

    :ambulance: Critical hotfix, parameters treatment bug

--- classes/DAO/Support/Parameters.php
+++ classes/DAO/Support/Parameters.php
@@ -7,7 +7,12 @@ class Parameters
     public static function set($statement, array $parameters = [])
     {
         foreach ($parameters as $key => $value) {
-            $statement->bindParam($key, $value);
+            self::binder($statement, $key, $value);
         }
+    }
+
+    private static function binder($statement, $key, $value)
+    {
+        $statement->bindParam($key, $value);
     }
 }
