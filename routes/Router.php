5ce8a25048a44bc159fb46c4994ae18e8a6fbf4b
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-15 14:26:03 -0300

    :sparkles: Using the Api Controller to manage methods

--- routes/Router.php
+++ routes/Router.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use Access\Controller\UserController;
+use Access\Controller\ApiController;
 
 class Router
 {
@@ -15,13 +15,7 @@ class Router
     {
         $app = AppFactory::create();
 
-        $app->get('/', function ($request, $response, $arguments) {
-            $user = new UserController;
-
-            echo json_encode($user->index());
-
-            exit();
-        });
+        $app->get('/', ApiController::class . ':index');
 
         $app->run();
     }
