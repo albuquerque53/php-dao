cff753162db1b8ab51c247f25f1f06f74a348db4
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-15 13:36:10 -0300

    :boom: Using the Router class on index.php

--- index.php
+++ index.php
@@ -1,51 +1,8 @@
 <?php
 
-use Access\Controller\UserController;
+use Routes\Router;
 
 require_once 'vendor/autoload.php';
 
-// Controller Instance
-$user = new UserController();
-
-/*
-// All users
-$list = $user->index();
-
-echo json_encode($list);
-*/
-
-/*
-// Get by ID
-$userOne = $user->show(1);
-  
-echo json_encode($userOne);
-*/
-
-/*
-// Search user by login
-$search = $user->search('ro');
-
-echo json_encode($search);
- */
-
-/*
-// Insert new user
-$user->store('John Wick', 'you-killed-ma-dog!!!');
-
-echo json_encode($user->index()); // Check
- */
-
-/*
-// Update user
-$user->update(2, 'John Wick', 'you-killed-ma-dog!!!');
-
-echo json_encode($user->index());
-*/
-
-/*
-// Delete
-$user->destroy(7);
-
-echo json_encode($user->index()); // Check
-*/
+Router::run();
 
