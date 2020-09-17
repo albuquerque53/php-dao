72d780e5dc53dd01fac3cdc7deb8d97a7184c75a
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-17 18:22:07 -0300

    :wrench: Application configuration

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
-use Routes\Router;
+use Src\Routes\Router;
+use Src\App;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = Router::createApp();
+$app = new App;
+$router = new Router;
+
+$app->implementRoutes($router);
 $app->run();
 
