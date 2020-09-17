17dde59d92518133e6f16ae77e8b4d64ea74bb97
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-17 18:19:06 -0300

    :sparkles: Src folder with App class

---
+++ src/App.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Src;
+
+use Slim\Factory\AppFactory;
+use Src\Routes\Router;
+
+class App extends \Slim\App 
+{
+    protected $app;
+
+    public function __construct()
+    {
+        $this->app = AppFactory::create(); 
+
+        return $this->app;
+    }
+
+    public function implementRoutes(Router $router)
+    {
+        $router::routerApp($this->app);
+    }
+}
