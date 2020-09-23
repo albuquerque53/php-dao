f0e47c324cd2c2ae65c7bfcb0e3159525da4aec6
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-23 16:06:11 -0300

    :bug: Fixing the vendor path

--- src/Routes/Router.php
+++ src/Routes/Router.php
@@ -2,7 +2,7 @@
 
 namespace Src\Routes;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use Access\Controller\ApiController;
