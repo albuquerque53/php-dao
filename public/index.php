087d08ff096e1c6f41dc7b98cad8b942d6418688
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-23 16:08:21 -0300

    :sparkles: Using routedApp instead of app

--- public/index.php
+++ public/index.php
@@ -8,6 +8,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new App;
 $router = new Router;
 
-$app->implementRoutes($router);
-$app->run();
+$routedApp = $app->implementRoutes($router);
+$routedApp->run();
 
