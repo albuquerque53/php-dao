260206027c749472432be712d4b3a4c0ebc2fc38
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-09-25 14:20:22 -0300

    :bug: Fixed env file

--- .env-example.php
+++ .env-example.php
@@ -1,8 +1,8 @@
 <?php
 
-putenv('DATABASE' . '');
-putenv('HOST' . '');
-putenv('SCHEMA_NAME' . '');
-putenv('DB_LOGIN' . '');
-putenv('DB_PASSWORD' . '');
+putenv('DATABASE=' . '');
+putenv('HOST=' . '');
+putenv('SCHEMA_NAME=' . '');
+putenv('DB_LOGIN=' . '');
+putenv('DB_PASSWORD=' . '');
 
